8cb38cf673b1351931a3b379d6a17122b9a5f72c
Chuck Konkol <ckonkol@gmail.com>
2021-02-02 11:37:24 -0600

    update

--- app/src/main/java/edu/student/week3/MainActivity.kt
+++ app/src/main/java/edu/student/week3/MainActivity.kt
@@ -1,6 +1,5 @@
 package edu.student.week3
 
-import android.annotation.SuppressLint
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,10 +9,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContextCompat.getSystemService
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
